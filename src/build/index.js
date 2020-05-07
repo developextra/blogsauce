2e225a8ece43d35fb2a5af4f179c3a16d08312da
arakilian0 <arakilian0@gmail.com>
2020-05-06 23:07:03 -0700

    UPDATE testing post module

--- src/build/index.js
+++ src/build/index.js
@@ -1,16 +1,26 @@
-const fs = require('fs')
-const path = require('path')
+// const fs = require('fs')
+// const path = require('path')
 const snippet = require('./snippet')
+const post = require('./post')
 
 module.exports = function() {
-    let ext = `.${this.config.format}`
-    let entry = path.resolve(this.config.entry)
-    let output = path.resolve(this.config.output)
-    let snippets = snippet(this.config.snippet)
+    // let ext = `.${this.config.format}`
+    // let entry = path.resolve(this.config.entry)
+    // let output = path.resolve(this.config.output)
+    let posts = post(this.config, snippet(this.config.snippet))
 
-    console.log(entry)
-    console.log(output)
-    console.log(ext)
-    console.log(snippets)
-    // console.log(this.config.snippet)
+    // switch(ext) {
+    //     case '.html':
+    //         console.log('handle html format')
+    //         break
+    //     case '.md':
+    //         console.log('handle md format')
+    //         break
+    // }
+
+    // console.log(snippets)
+    console.log(posts)
+    // console.log(output)
+    // console.log(ext)
+    // console.log(this.config.format)
 }
