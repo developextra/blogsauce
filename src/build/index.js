178f3783b61789dfe5b079e19e32ee88f7b1525f
arakilian0 <arakilian0@gmail.com>
2020-05-09 12:17:08 -0700

    UPDATE minor edits to build

--- src/build/index.js
+++ src/build/index.js
@@ -1,18 +1,27 @@
 const post = require('./post')
 const snippet = require('./snippet')
 
+function buildAll(_this) {
+    console.log('building all')
+    console.log(_this)
+}
+
+function buildIndex(_this) {
+    console.log('building index')
+    console.log(_this)
+}
+
+function buildAuthor(_this) {
+    console.log('building author')
+    console.log(_this)
+}
+
 module.exports = function(target='all') {
     if(!target) return this.error('Valid Target Required::build')
     switch(target) {
-        case 'all':
-            console.log('building all')
-            break
-        case 'index':
-            console.log('building index')
-            break
-        case 'author':
-            console.log('building author')
-            break
+        case 'all': buildAll(this); break
+        case 'index': buildIndex(this); break
+        case 'author': buildAuthor(this); break
         default: this.error('Valid Target Required::build(invalid)')
     }
 }
