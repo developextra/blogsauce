34a5264989e0aaecaac48935deaf98970546c3a0
arakilian0 <arakilian0@gmail.com>
2020-05-12 19:27:35 -0700

    Update test script

--- test/index.js
+++ test/index.js
@@ -2,15 +2,9 @@ const blog = require('../src')
 
 blog.setConfig({
     entry: 'posts',
-    prefer: {
-        post: '.markdown',
-        snippet: '.json',
-        boilerplate: true,
-        smart: false
-    },
     snippet: {
         entry: 'snippets'
     }
 })
 
-blog.new('Tesdsdst')
+blog.build('Hello World')
